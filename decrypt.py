a907819c8a10a4269cf2b41780ff5f66cff1c401
unknown <anonuevoloraine6@gmail.com>
2023-04-17 22:07:37 +0800

    header and request of user's name

---
+++ decrypt.py
@@ -0,0 +1,19 @@
+#Anonuevo, Loraine N.
+#BSCPE 1-4
+#Problem 1
+
+#Header
+print("\033[;33;1;3m\033[10m" * 65)
+print("\033[;33;1;3mAyooo! It's a pleasure to have you here!\033[0m".center(81))
+print("\033[;33;1;3mಠ\033[10m" * 65)
+
+#Request about the user's name.
+print("")
+print("\033[1;3mMy name is \033[;45;1;3mYnah\033[0m")
+your_name = input("\033[1;3mWhat is your name?\033[0m")
+print("\033[;1;3mI'm proud of you!\033[;34;1;3m" + your_name + "\033[0m \033[;1;3m, and i'll share some knowledge today!\033[0m")
+
+print("")
+print("\033[;45;1;3m*\033[0m" * 70)
+print("\033[;1;3mChill and do amazing stuff with me!\033[0m".center(81))
+print("\033[;45;1;3m*\033[0m" * 70)
